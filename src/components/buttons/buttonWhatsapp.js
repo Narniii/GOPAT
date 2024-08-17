f2b790205077cbb2c4c5490bec6a730ec78998ba
Narniii <narniii@yahoo.com>
2024-08-17 21:00:19 +0330

    text via whatsapp

--- src/components/buttons/buttonWhatsapp.js
+++ src/components/buttons/buttonWhatsapp.js
@@ -2,32 +2,45 @@ import { Box, Button, Typography, keyframes } from "@mui/material";
 import shoppingBag from '../../assets/shoppingBag.svg'
 
 const ButtonWhatsapp = ({ text, action, icon, width, mb, maxwidth }) => {
-    return (<Box
-        // id="fill"
-        sx={{
-            backgroundColor: '#25D366', cursor: 'pointer', color: 'white', boxSizing: 'border-box',
-            height: { xs: '50px', md: '45px' }, mb: mb ? mb : '0',
-            width: width ? width : 'max-content', maxWidth: maxwidth ? maxwidth : 'unset',
-            px: { xs: '8px', sm: '16px', md: '32px' }, gap: '8px',
-            display: 'flex', alignItems: 'center', transition: '500ms ease', justifyContent: 'space-between',
-            '&:hover': {
-                backgroundColor: '#999999',
-            }
+    return (
+        <a
+            href="https://wa.me/+989106741971"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+                textDecoration: 'none',
+                width: width ? width : 'max-content', maxWidth: maxwidth ? maxwidth : 'unset',
+                height: 'max-content'
+            }}
+        >
 
-        }}
-        onClick={action}>
-        <Box sx={{
-            backgroundPosition: 'center',
-            backgroundImage: `url(${shoppingBag})`,
-            backgroundRepeat: 'no-repeat', backgroundSize: 'contain',
-            width: '24px',
-            height: '24px',
-        }} />
-        <Typography sx={{ fontSize: { xs: '14px', sm: '16px', md: '16px' }, }}>
-            {text}
-        </Typography>
-        <Box />
-    </Box>);
+            <Box
+                // id="fill"
+                sx={{
+                    backgroundColor: '#25D366', cursor: 'pointer', color: 'white', boxSizing: 'border-box',
+                    height: { xs: '50px', md: '45px' }, mb: mb ? mb : '0',
+                    width: width ? width : 'max-content', maxWidth: maxwidth ? maxwidth : 'unset',
+                    px: { xs: '8px', sm: '16px', md: '32px' }, gap: '8px',
+                    display: 'flex', alignItems: 'center', transition: '500ms ease', justifyContent: 'space-between',
+                    '&:hover': {
+                        backgroundColor: '#999999',
+                    }
+
+                }}
+                onClick={action}>
+                <Box sx={{
+                    backgroundPosition: 'center',
+                    backgroundImage: `url(${shoppingBag})`,
+                    backgroundRepeat: 'no-repeat', backgroundSize: 'contain',
+                    width: '24px',
+                    height: '24px',
+                }} />
+                <Typography sx={{ fontSize: { xs: '14px', sm: '16px', md: '16px' }, }}>
+                    {text}
+                </Typography>
+                <Box />
+            </Box>
+        </a>);
 }
 
 export default ButtonWhatsapp;
--- src/components/buttons/buttonWhatsappContact.js
+++ src/components/buttons/buttonWhatsappContact.js
@@ -3,31 +3,34 @@ import shoppingBag from '../../assets/shoppingBag.svg'
 
 const ButtonWhatsappContact = ({ text, action, icon, width, mb, maxwidth }) => {
     return (
-        // <a
-        //     href="https://wa.me/00989365477769"
-        //     class="whatsapp_float"
-        //     target="_blank"
-        //     rel="noopener noreferrer"
-        // >
-
-        <Box
-            sx={{
-                backgroundColor: '#25D366', cursor: 'pointer', color: 'white', boxSizing: 'border-box',
-                height: { xs: '50px', md: '60px' }, fontWeight: 500,
+        <a
+            href="https://wa.me/+989106741971"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+                textDecoration: 'none',
                 width: width ? width : 'max-content', maxWidth: maxwidth ? maxwidth : 'unset',
-                px: { xs: '8px', sm: '16px', md: '32px' }, mb: mb ? mb : 'unset',
-                display: 'flex', alignItems: 'center', textTransform: 'capitalize', transition: '500ms ease',
-                '&:hover': {
-                    backgroundColor: '#999999',
-                }
-
+                height: 'max-content'
             }}
-            onClick={action}>
-            <Typography sx={{ fontSize: { xs: '14px', sm: '16px', md: '16px' }, width: '100%', textAlign: 'center' }}>
-                {text}
-            </Typography>
-        </Box>
-        // </a>
+        >
+            <Box
+                sx={{
+                    backgroundColor: '#25D366', cursor: 'pointer', color: 'white', boxSizing: 'border-box',
+                    height: { xs: '50px', md: '60px' }, fontWeight: 500,
+                    width: width ? width : 'max-content', maxWidth: maxwidth ? maxwidth : 'unset',
+                    px: { xs: '8px', sm: '16px', md: '32px' }, mb: mb ? mb : 'unset',
+                    display: 'flex', alignItems: 'center', textTransform: 'capitalize', transition: '500ms ease',
+                    '&:hover': {
+                        backgroundColor: '#999999',
+                    }
+
+                }}
+                onClick={action}>
+                <Typography sx={{ fontSize: { xs: '14px', sm: '16px', md: '16px' }, width: '100%', textAlign: 'center' }}>
+                    {text}
+                </Typography>
+            </Box>
+        </a>
     );
 }
 
